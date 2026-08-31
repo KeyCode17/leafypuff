75b65d4329287972a3a5909f2bb26fc93e55b600
KeyCode17 <m.daffa.karyudi@gmail.com>
2026-08-31 18:17:32 +0700

    feat(nix): package the api as a flake output

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "leafyPuff sync API";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.leafypuff-api = pkgs.rustPlatform.buildRustPackage {
+          pname = "leafypuff-api";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          # The workspace also holds the Android-facing core crate; only the API is deployable.
+          cargoBuildFlags = [ "-p" "leafypuff-api" ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+          doCheck = false;
+        };
+
+        packages.default = self.packages.${system}.leafypuff-api;
+      }
+    );
+}
