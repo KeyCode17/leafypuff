3312a7e9160c6495419e6be91e5fba191e673004
KeyCode17 <m.daffa.karyudi@gmail.com>
2026-09-01 03:55:09 +0700

    feat(core): derive the master key from a passphrase with argon2id

--- apps/core/src/domain/crypto/mod.rs
+++ apps/core/src/domain/crypto/mod.rs
@@ -2,10 +2,12 @@ pub mod aad;
 pub mod error;
 pub mod keys;
 pub mod padding;
+pub mod passphrase;
 pub mod seal;
 
 pub use aad::FieldContext;
 pub use aad::{FIELD_BODY, FIELD_COVER, FIELD_PHOTO, FIELD_TITLE};
 pub use error::CryptoError;
 pub use keys::{ContentKey, MasterKey, RecoveryKey};
+pub use passphrase::{derive_master_key, generate_salt};
 pub use seal::{SealedField, open, seal};
